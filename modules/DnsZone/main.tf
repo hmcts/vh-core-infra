59ca293a8ea8d161e67889eae8f836ffc3581233
Ryan Bartram <ryan@dftai.ch>
2019-10-30 22:39:10 +0000

    increase DNS TTL to 3600

--- modules/DnsZone/main.tf
+++ modules/DnsZone/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_dns_a_record" "vh" {
   name                = each.value.name
   zone_name           = var.zone_name
   resource_group_name = var.resource_group_name
-  ttl                 = 300
+  ttl                 = 3600
   records             = [each.value.value]
 }
 
@@ -14,6 +14,6 @@ resource "azurerm_dns_cname_record" "vh" {
   name                = each.value.name
   zone_name           = var.zone_name
   resource_group_name = var.resource_group_name
-  ttl                 = 300
+  ttl                 = 3600
   record             = each.value.value
 }
