eb807895be2bf73f89ad6426c97b340df80ff6c4
Ryan Bartram <ryan@dftai.ch>
2019-11-07 14:41:45 +0000

    pass build agent value from pipeline variables

--- terraform/inputs.tf
+++ terraform/inputs.tf
@@ -5,7 +5,7 @@ variable "location" {
 
 variable "build_agent_vnet" {
     type = "string"
-    default = "/subscriptions/705b2731-0e0b-4df7-8630-95f157f0a347/resourceGroups/vh-devtestlabs-dev/providers/Microsoft.Network/virtualNetworks/Dtlvh-devtestlabs-dev/subnets/Dtlvh-devtestlabs-devSubnet"
+    default = ""
 }
 
 variable "idam_tenant_id" {
---
+++ terraform/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "azurerm" {
+    container_name = "tfstate"
+    key            = "infra/vh-core-infra.tfstate"
+  }
+
+  required_version = ">= 0.12"
+  required_providers {
+    azurerm = ">= 1.36"
+    azuread = "~> 0.6"
+  }
+}
