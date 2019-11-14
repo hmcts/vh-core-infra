d928f2df7d32fd027b1bd71d0b3ab9f57c445f75
Ryan Bartram <ryan@dftai.ch>
2019-11-14 15:54:49 +0000

    enable implicit flow for frontend apps

--- terraform/modules/AppRegistrations/main.tf
+++ terraform/modules/AppRegistrations/main.tf
@@ -10,7 +10,7 @@ resource "azuread_application" "vh" {
   identifier_uris            = [each.value.url]
   reply_urls                 = terraform.workspace == "Dev" ? concat(local.localhostreply, [each.value.url, "${each.value.url}/login", "${each.value.url}/home"]) : [each.value.url, "${each.value.url}/login", "${each.value.url}/home"]
   available_to_other_tenants = false
-  oauth2_allow_implicit_flow = false
+  oauth2_allow_implicit_flow = lookup(local.oauth2_allow_implicit_flow, each.key, false)
   type                       = "webapp/api"
   public_client              = false
   group_membership_claims    = "None"
@@ -43,7 +43,7 @@ resource "random_password" "password" {
 resource "azuread_application_password" "vh" {
   for_each = var.apps
 
-  key_id                = uuidv5("url",each.value.url)
+  key_id                = uuidv5("url", each.value.url)
   application_object_id = azuread_application.vh[each.key].object_id
   value                 = random_password.password[each.key].result
   end_date_relative     = "8760h"
