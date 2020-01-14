92108f9b4bac68793897fa32bb3b95d462c877d7
Ryan Bartram <ryan@dftai.ch>
2020-01-14 22:05:40 +0000

    fix for app config SKU

--- terraform/modules/AppConfiguration/main.tf
+++ terraform/modules/AppConfiguration/main.tf
@@ -7,5 +7,5 @@ resource "azurerm_app_configuration" "vh" {
   resource_group_name = data.azurerm_resource_group.vh-core-infra.name
   location            = "uksouth"
 
-  sku = local.sku.name
+  sku = local.sku
 }
